faafe022a36edbe0104dbc2ec113b74336cb0e35
Nyotron <33634364+Nyotron@users.noreply.github.com>
2019-11-22 14:02:26 -0800

    Update RIPlacePOC.cpp
    Changed the DOS device name's path prefix to "\\.\" (Win32 device namespace), as the previous fails on XP.

--- RIPlacePOC.cpp
+++ RIPlacePOC.cpp
@@ -9,7 +9,7 @@ void Cleanup() {
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 	const wchar_t *dummyFilePath = LR"(C:\test\dummyfile.txt)";
 	const wchar_t *targetFilePath = LR"(\??\C:\test\target.txt)"; // make sure to create it beforehand
-	const wchar_t *dosdevPath = LR"(\??\RIPlace)";
+	const wchar_t *dosdevPath = LR"(\\.\RIPlace)";
 	
 	HANDLE hDummy = CreateFileW(dummyFilePath, GENERIC_WRITE, 0, 0, CREATE_NEW, 0, 0); // change CREATE_NEW to CREATE_ALWAYS if you live on the edge
 	if (hDummy == INVALID_HANDLE_VALUE) {
